b397f537ff7aa709903315abebeed442db0f6add
Gibbun <coobs@outlook.com>
2017-11-03 13:06:02 -0500

    Update PkgDistributionCreator.py

--- MTM_Installer/PkgDistributionCreator.py
+++ MTM_Installer/PkgDistributionCreator.py
@@ -115,15 +115,12 @@ class PkgDistributionCreator(Processor):
                 % (e.errno, e.strerror))
         if p.returncode != 0:
             raise ProcessorError("cmmac conversion of %s failed: %s"
-                % (self.env['output_file'], err))
-        tree = ET.parse('/Users/Shared/AutoPkg/Cache/com.github.Gibbun.pkg.UofI_MTM_Installer/distribution.xml')
-        root = tree.getroot()
-        child = ET.SubElement(root, 'title')
-        child.text = 'My Awesome App'
-        newtree = root.getroottree()
-        print(type(newtree))
-        newtree.write('/Users/Shared/AutoPkg/Cache/com.github.Gibbun.pkg.UofI_MTM_Installer/distribution.xml') 
-    
+                % (self.env['output_file'], err))        
+        origtree = xml.etree.ElementTree.parse('/Users/Shared/AutoPkg/Cache/com.github.Gibbun.pkg.UofI_MTM_Installer/distribution.xml')
+        new_tag = xml.etree.ElementTree.SubElement(origtree.getroot(), 'title')
+        new_tag.text = 'Univ. of Illinois Munki Onboarding'
+        origtree.write('/Users/Shared/AutoPkg/Cache/com.github.Gibbun.pkg.UofI_MTM_Installer/distribution.xml')
+        
     def main(self):
         if os.path.exists(self.env['source_file1']):
             try:
