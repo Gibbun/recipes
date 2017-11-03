75064c894b0ccf8bb5a0af0777c51c69fa1aa48e
Gibbun <coobs@outlook.com>
2017-11-03 12:50:35 -0500

    Update PkgDistributionCreator.py

--- MTM_Installer/PkgDistributionCreator.py
+++ MTM_Installer/PkgDistributionCreator.py
@@ -120,7 +120,7 @@ class PkgDistributionCreator(Processor):
         root = tree.getroot()
         child = ET.SubElement(root, 'title')
         child.text = 'My Awesome App'
-        et.write('/Users/Shared/AutoPkg/Cache/com.github.Gibbun.pkg.UofI_MTM_Installer/distribution.xml')
+        ET.write('/Users/Shared/AutoPkg/Cache/com.github.Gibbun.pkg.UofI_MTM_Installer/distribution.xml')
     
     def main(self):
         if os.path.exists(self.env['source_file1']):
